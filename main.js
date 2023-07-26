e6e6138d30a2ab388530bdfbb83f6b962ec690a2
JaBoe97 <boehler.jana@gmail.com>
2023-07-26 10:19:29 +0200

    hover effect and links for product images; responsive product images
    onclick for product images to be sent to live pages

---
+++ main.js
@@ -0,0 +1,17 @@
+/* onclick events for product images to be sent to live pages */
+
+function openLiveSiteNotification() {
+  window.location = "https://jaboe97.github.io/notifications_page";
+}
+
+function openLiveSiteResultSummary() {
+  window.location = "https://jaboe97.github.io/result_summary_component";
+}
+
+function openLiveSiteFourCard() {
+  window.location = "https://jaboe97.github.io/four_card_feature_section";
+}
+
+function openLiveSiteECommerce() {
+  window.location = "https://jaboe97.github.io/ecommerce_product_page";
+}
